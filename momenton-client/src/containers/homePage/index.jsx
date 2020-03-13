27d636952876dca9dcf3eff5af4a467badc267d7
golashr <rahul.g@ledgerium.net>
2020-03-13 17:38:27 +1100

    treeview updated

--- momenton-client/src/containers/homePage/index.jsx
+++ momenton-client/src/containers/homePage/index.jsx
@@ -5,8 +5,30 @@ import {
   momentonServerRequestFailed
 } from './homePage.actions';
 import { getEmployees } from '../../api/employee';
-import { rejigData } from '../../utils/treeHireracy';
 import MuiTreeView from 'material-ui-treeview';
+
+const tree = [
+  {
+    value: 'Parent A',
+    nodes: [{ value: 'Child A' }, { value: 'Child B' }]
+  },
+  {
+    value: 'Parent B',
+    nodes: [
+      {
+        value: 'Child C'
+      },
+      {
+        value: 'Parent C',
+        nodes: [
+          { value: 'Child D' },
+          { value: 'Child E' },
+          { value: 'Child F' }
+        ]
+      }
+    ]
+  }
+];
 
 class HomePage extends React.Component {
   constructor(props) {
@@ -18,12 +40,36 @@ class HomePage extends React.Component {
   async componentDidMount() {
     getEmployees()
       .then(response => {
-        console.log(`[+] Response is received ${response}`);
-        this.setState({ treeView: rejigData(response) });
-        console.log('this.state.treeView', this.state.treeView);
-        if (response.success)
-          this.props.momentonServerRequestSuccess(response.data);
-        else this.props.momentonServerRequestFailed(response);
+        console.log(`[+] Response is received ${JSON.stringify(response)}`);
+        var employeeData = [];
+        var parent = {};
+        response.forEach(data => {
+          console.log(data.ID, data.employeeName, data.reportees.length);
+          parent.value = data.employeeName;
+          parent.nodes = [];
+          data.reportees.forEach(reporteeOneLevel => {
+            var childOneLevel = {};
+            childOneLevel.value = reporteeOneLevel.employeeName;
+            childOneLevel.nodes = [];
+            reporteeOneLevel.reportees.forEach(reporteeSecondLevel => {
+              var childSecondLevel = {};
+              childSecondLevel.value = reporteeSecondLevel.employeeName;
+              childSecondLevel.nodes = [];
+              childOneLevel.nodes.push(childSecondLevel);
+            });
+            parent.nodes.push(childOneLevel);
+          });
+          employeeData.push(parent);
+        });
+        this.setState({ treeView: employeeData });
+        console.log(
+          `[+] Updated this.state.treeview is ${JSON.stringify(
+            this.state.treeView
+          )}`
+        );
+        if (response.length)
+          this.props.momentonServerRequestSuccess(this.state.treeView);
+        else this.props.momentonServerRequestFailed(employeeData);
       })
       .catch(error => {
         console.log(
@@ -33,10 +79,18 @@ class HomePage extends React.Component {
   }
 
   render() {
+    console.log(
+      `[+] Updated this.state.treeview is ${JSON.stringify(
+        this.state.treeView
+      )}`
+    );
+    console.log(`[+] fixed tree is ${JSON.stringify(tree)}`);
     return (
       <div>
         {/* <MuiTreeView tree={this.state.treeView} /> */}
         <h1> Welcome to Momenton Client App </h1>
+        <MuiTreeView tree={tree} />
+        {/* <MuiTreeView tree={this.state.treeView} /> */}
       </div>
     );
   }
